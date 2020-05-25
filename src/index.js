5bc81b4335a89f2f4e10503d718a6fd5167ff840
Odohi David <odohi.david@gmail.com>
2020-05-25 16:33:04 +0100

    add comment

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import { hydrate } from "redux-easy-persist";
 import { Provider } from 'react-redux';
 import configureStore from './store';
 
+// get the store
 let store = configureStore();
 
 // this would retrieve the store from persistent storage
