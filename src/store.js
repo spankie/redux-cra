14df30c424c1c074e3f0ac9a940b170e50368485
Odohi David <odohi.david@gmail.com>
2020-05-25 14:28:05 +0100

    create store with persistence

---
+++ src/store.js
@@ -0,0 +1,36 @@
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import rootReducer from './reducers/rootReducer';
+import createPersistor from "redux-easy-persist";
+
+const AsyncStorage = {
+  getItem: (key) => {
+    return new Promise((resolve, reject) => {
+      const item = localStorage.getItem(key);
+      if (!item) {
+        return reject("no value");
+      }
+      return resolve(item);
+    });
+  },
+  setItem: (key, value) => {
+    return new Promise((resolve, reject) => {
+      try {
+        localStorage.setItem(key, value);
+        resolve(key + " set successfully");
+      }catch(error) {
+        reject("something went wrong");
+      }
+    })
+  }
+}
+
+// create a persistor and pass in storeEngine and persistKey
+const persistor = createPersistor({
+  storeEngine: AsyncStorage,
+  persistKey: 'rootState',
+});
+
+export default function configureStore(initialState={}) {
+ return createStore(rootReducer, initialState, applyMiddleware(persistor,thunk));
+}
